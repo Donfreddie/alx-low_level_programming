a7976a32620fd3e86376b96114acfc657934f168
Donfreddie <wilfred.okon@gmail.comls>
2023-03-18 23:50:54 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 /**
- * main: - main functions
- * return: Always 0 (Success)
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "with proper grammar, but the outcome is a piece of art
+ * Return: 0
  */
 int main(void)
 {
