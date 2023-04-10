3620832e162d0d3e0373d7f2061730264e4145cc
Donfreddie <wilfred.okon@gmail.comls>
2023-04-10 10:41:33 -0700

    2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,13 +1,11 @@
 #include "main.h"
-#include <stddef.h> 
+#include <stddef.h>
 /**
 *_strchr - Returns a pointer to the first occurrence
 *of the character c in the string s, or NULL if the
 *character is not found
-*
 *@s:string targeted
 *@c:character targeted
-*
 *Return: returns pointer to first occcurence of c
 */
 char *_strchr(char *s, char c)
@@ -15,9 +13,9 @@ char *_strchr(char *s, char c)
 	int i;
 
 	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
-		;
+	;
 	if (s[i] == c)
-		return (s + i);
+	return (s + i);
 	else
-		return (NULL);
+	return (NULL);
 }
