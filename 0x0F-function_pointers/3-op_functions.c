0eba6bfdedad442e5646eaf452e315a52add0375
Donfreddie <wilfred.okon@gmail.comls>
2023-04-20 15:03:25 -0700

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+/**
+ *  * op_add - sum parameter numbers
+ *   *
+ *    * @a: integer
+ *     * @b: integer
+ *      *
+ *       * Return: result sum operation
+ */
+int op_add(int a, int b)
+{
+		return (a + b);
+}
+/**
+ *  * op_sub - substract parameter numbers
+ *   *
+ *    * @a: integer
+ *     * @b: integer
+ *      *
+ *       * Return: result substract operation
+ */
+int op_sub(int a, int b)
+{
+		return (a - b);
+}
+/**
+ *  * op_mul - multiply parameter numbers
+ *   *
+ *    * @a: integer
+ *     * @b: integer
+ *      *
+ *       * Return: result multiply operation
+ */
+int op_mul(int a, int b)
+{
+		return (a * b);
+}
+/**
+ *  * op_div - divide parameter numbers
+ *   *
+ *    * @a: integer
+ *     * @b: integer
+ *      *
+ *       * Return: result divide operation
+  */
+int op_div(int a, int b)
+{
+		return (a / b);
+}
+/**
+ *  * op_mod - reminder divide parameter numbers
+ *   *
+ *    * @a: integer
+ *     * @b: integer
+ *      *
+ *       * Return: result mod operation
+ */
+int op_mod(int a, int b)
+{
+		return (a % b);
+}
